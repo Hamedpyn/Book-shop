f03cbaa3fb00af1422482bd0bb7eff6edf0ba60d
Hamed <hamedpyn@gmail.com>
2023-08-28 05:16:28 +0430

    select dom elements

---
+++ assets/src/js/index.js
@@ -0,0 +1,13 @@
+// Select DOM Elements
+const booksLi = document.querySelector('#books');
+const watchBookPage = document.querySelector('#watch-BookPage');
+const container = document.querySelector('#container');
+const booksPageContainer = document.querySelector('#booksPageContainer');
+const spinner = document.querySelector('#spinner');
+const thisBookPage = document.querySelector('#thisBookPage');
+const thisFirstPage = document.querySelector('#thisFirstPage');
+const firstPage = document.querySelector('#firstPage');
+const allBtn = document.querySelector('#allBtn');
+const bodyEl = document.body;
+const booksObject = document.querySelector("#booksObject");
+
