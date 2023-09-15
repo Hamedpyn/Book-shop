2d636621d423c1b73f696a3b2af27600aad003e0
Hamed <hamedpyn@gmail.com>
2023-09-15 09:23:03 +0430

    set the pagination and making the click option on

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -587,4 +587,8 @@ const swiper = new Swiper(".swiper", {
         nextEl:".swiper-button-next",
         prevEl:".swiper-button-prev",
     },
+    pagination:{
+        clickable:true,
+        el:".swiper-pagination"
+    },
 })
